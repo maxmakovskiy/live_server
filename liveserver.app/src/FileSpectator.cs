1e94240539f920cd57070e9e55b7d5a80edad82c
maxmakovskiy <makovskiymax@gmail.com>
2022-01-30 15:20:24 +0200

    working with files and unit-tests

--- liveserver.app/src/FileSpectator.cs
+++ liveserver.app/src/FileSpectator.cs
@@ -1,17 +1,60 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
 namespace liverserver.app {
 
 public class FileSpectator {
 
-    public static string[] AbsolutePathToFiles(string target)
+    public static string[] AbsolutePathToFiles(string targetDir)
     {
         var result = new List<string>();
 
-        foreach(string filename in Directory.GetFiles(target, "", SearchOption.AllDirectories)) 
+        foreach(string filename in Directory.GetFiles(targetDir, "", SearchOption.AllDirectories)) 
         {
             result.Add(filename);
         }
 
         return result.ToArray();
+    }
+
+    public static string LoadAndInject(string filename) 
+    {
+        string injection = LoadFileAsOneliner("/home/xemerius/devs/live_server/liveserver.app/src/to_inject.js");
+
+        var result = new List<string>();
+        foreach(string line in File.ReadAllLines(filename)) 
+        {
+            if (Regex.IsMatch(line, $"<body>.*</body>")) {
+                string internalContent = line.Trim(new char[] {'<', '>', 'b', 'o', 'd', 'y', '/'});
+                result.Add("<body>");
+                result.Add(internalContent);
+                result.Add(injection);
+                result.Add("</body>");
+                continue;
+            }
+            
+            if (Regex.IsMatch(line, $".*</body>.*")) {
+                result.Add("<script>");
+                result.Add(injection);
+                result.Add("</script>");
+            }
+
+            result.Add(line.Trim(' '));
+        }
+
+        return String.Join("\n", result.ToArray());
+    }
+
+    public static string LoadFileAsOneliner(string filename) 
+    {
+        string result = "";
+        foreach(string line in File.ReadAllLines(filename))
+        {
+            result += line.Trim(' ');
+        }
+
+        return result;
     }
 
 }
--- liveserver.test/src/FileSpectatorTest.cs
+++ liveserver.test/src/FileSpectatorTest.cs
@@ -12,7 +12,7 @@ public class FileSpectatorTest {
     public void TestAbsolutePathToFiles()
     {
         string root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-        string target = root + "/rcs";
+        string target = root + "/rcs/observableDirs";
         string[] result = FileSpectator.AbsolutePathToFiles(target);
         string[] expected = {
             target + "/file1.txt",
@@ -26,6 +26,29 @@ public class FileSpectatorTest {
         Assert.Equal(expected, result);
     }
 
+    [Fact]
+    public void TestInjection()
+    {
+        string root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        string target = root + "/rcs/index2.html";
+        string[] result = FileSpectator.LoadAndInject(target).Split('\n');
+        string[] expected = {
+            "<!DOCTYPE html>",
+            "<html>",
+            "<head>",
+            "<meta charset='utf8'>",
+            "</head>",
+            "<body>",
+            "Hello world!",
+            "<script>",
+            "(function() {var url = 'ws://localhost:80/';var output;function init() {output = document.getElementById('output');doWebSocket();}function doWebSocket() {websocket = new WebSocket(url);websocket.onopen = function(e) {onOpen(e);};websocket.onmessage = function(e) {onMessage(e);};}function onOpen(event) {writeToScreen('CONNECTED');send('WebSocket rocks');}function onMessage(event) {writeToScreen('RECEIVE: ' + event.data);}function send(message) {websocket.send(message);}function writeToScreen(message) {var pre = document.createElement('p');pre.innerHTML = message;output.appendChild(pre);}window.addEventListener('load', init, false);})();",
+            "</script>",
+            "</body>",
+            "</html>"     
+        };
+
+        Assert.Equal(expected, result);
+    }
 
 
 }
